39861b00be581f198d1fb4cbe59ae02534b8b735
Developlee20 <developlee20@gmail.com>
2025-10-23 15:12:06 +0900

    Feat: 베이스 Response 생성

---
+++ app/base/base_response.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel, Field
+from typing import TypeVar, Generic
+
+typeT = TypeVar('T')
+
+class BaseResponse(BaseModel):
+    status_code: int = Field(..., description="상태 코드")
+    detail: str = Field(..., description="상세 메시지")
+    data: typeT = Field(..., description="응답 데이터")
